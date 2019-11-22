deb7e3dc5ab120dbd34ccfb744a573f06cca2dd0
Perkins <juston.perkins@williams.com>
2019-11-22 09:32:45 -0700

    first get request

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -19,7 +19,7 @@ class Blog extends Component {
                 const updatedPosts = posts.map(post => {
                     return {
                         ...post,
-                        author: 'Max'
+                        author: 'Juston'
                     }
                 })
                 this.setState({posts: updatedPosts});
