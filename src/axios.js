9b0f7f99e719230bb309e5c5777be907e87ffd9d
Perkins <juston.perkins@williams.com>
2019-12-09 10:39:39 -0700

    folder structure

--- src/axios.js
+++ src/axios.js
@@ -1,14 +1,3 @@
-// import axios from 'axios';
-
-// const instance = axios.create({
-//     baseURL: 'https://jsonplaceholder.typicode.com'
-// });
-
-// instance.defaults.headers.common['Authorization'] = 'AUTH TOKEN FROM INSTANCE';
-
-// // instance.interceptors.request...
-
-// export default instance;
 
 import axios from 'axios';
 
